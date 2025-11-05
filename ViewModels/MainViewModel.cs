f2e513c401b346bea93d79cd0edc5ec8a2603840
gauravjsh8 <gauravjsh8@gmail.com>
2025-11-05 20:20:42 +0200

    Refactored to MVVM pattern and added ViewModels

---
+++ ViewModels/MainViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows.Input;
+
+namespace LifeManagementApp.ViewModels
+{
+    public class MainViewModel : BindableObject
+    {
+        public ObservableCollection<Note> Notes { get; set; } = new ObservableCollection<Note>();
+
+        public ICommand AddNoteCommand { get; }
+        public ICommand AboutCommand { get; }
+        public ICommand RefreshCommand { get; }
+
+        public INavigation Navigation { get; set; }
+
+        public MainViewModel(INavigation navigation)
+        {
+            Navigation = navigation;
+            AddNoteCommand = new Command(OnAddNote);
+            AboutCommand = new Command(OnAbout);
+            RefreshCommand = new Command(LoadNotes);
+            LoadNotes();
+        }
+
+        private void LoadNotes()
+        {
+            Notes.Clear();
+            foreach (var note in Note.LoadAll().OrderByDescending(n => n.Date))
+                Notes.Add(note);
+        }
+
+        private async void OnAddNote()
+        {
+            await Navigation.PushAsync(new NotePage());
+        }
+
+        private async void OnAbout()
+        {
+            await Navigation.PushAsync(new AboutPage());
+        }
+    }
+}
---
+++ ViewModels/NoteViewModel.cs
@@ -0,0 +1,57 @@
+﻿using System.IO;
+using System.Windows.Input;
+
+namespace LifeManagementApp.ViewModels
+{
+    public class NoteViewModel : BindableObject
+    {
+        private Note currentNote;
+        private string text;
+
+        public string Text
+        {
+            get => text;
+            set
+            {
+                text = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand SaveCommand { get; }
+        public ICommand DeleteCommand { get; }
+
+        public INavigation Navigation { get; set; }
+
+        public NoteViewModel(INavigation navigation, Note note = null)
+        {
+            currentNote = note;
+            Navigation = navigation;
+
+            if (note != null)
+                Text = note.Text;
+
+            SaveCommand = new Command(OnSave);
+            DeleteCommand = new Command(OnDelete);
+        }
+
+        private async void OnSave()
+        {
+            if (string.IsNullOrWhiteSpace(Text))
+                return;
+
+            if (currentNote == null)
+                Note.Save(Text);
+            else
+                File.WriteAllText(currentNote.Filename, Text);
+
+            await Navigation.PopAsync();
+        }
+
+        private async void OnDelete()
+        {
+            currentNote?.Delete();
+            await Navigation.PopAsync();
+        }
+    }
+}
