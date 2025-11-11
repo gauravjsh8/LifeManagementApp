095478a1341b663e134e328e892af7a2b5adec52
gauravjsh8 <gauravjsh8@gmail.com>
2025-11-11 20:25:56 +0200

    Add joke api

---
+++ Models/Joke.cs
@@ -0,0 +1,20 @@
+﻿namespace LifeManagementApp.Models
+{
+    public class Joke
+    {
+        public string Content { get; set; } = string.Empty;
+
+        public override string ToString() => Content;
+    }
+
+    public class OneLinerJoke : Joke
+    { }
+
+    public class TwoLinerJoke : Joke
+    {
+        public string Setup { get; set; } = string.Empty;
+        public string Delivery { get; set; } = string.Empty;
+
+        public override string ToString() => $"{Setup} {Delivery}";
+    }
+}
